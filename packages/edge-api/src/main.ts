6612b7fde63b9065e47ebcbe037568dab2ffc0ea
Owen Delahoy <owen.delahoy@gmail.com>
2021-08-30 14:18:13 +0900

    feat: proxy H5P pages and inject script into HTML head

--- packages/edge-api/src/main.ts
+++ packages/edge-api/src/main.ts
@@ -2,9 +2,10 @@ import { error } from "./responses/error"
 import { json } from "./responses/json"
 import { redirectProtocol } from "./responses/redirect"
 import { statusText } from "./responses/statusText"
+import MimeType from "whatwg-mimetype"
 
 export default {
-  async fetch(request: Request, { rooms, activityStreams, ENVIRONMENT }: CloudflareEnvironment): Promise<Response> {
+  async fetch(request: Request, env: CloudflareEnvironment): Promise<Response> {
     const { headers } = request
     try {
       const url = new URL(request.url)
@@ -12,27 +13,56 @@ export default {
 
       const paths = url.pathname.split('/')
 
-      if(paths[1] !== "api") { return statusText(501) }
-
-      switch (paths[2]) {
-        case "request":
-          return json(request, 200, 2)
-        case "headers":
-          return json({...request.headers.entries()}, 200, 2)
-        case "room":
-          return fetchDurableObject(request, rooms, paths[3])
-        case "activity":
-          return fetchDurableObject(request, activityStreams, paths[3])
+      switch(paths[1]) {
+        case "api":
+          return api(request, env, paths)
+        case "h5p":
+          url.host = "h5p.kidsloop.live"
+          return pageStreamProxy(url.toString())
+        default:
+          return statusText(501)
       }
-
-      return statusText(404)
     } catch (e) {
-      return error(headers, e, ENVIRONMENT === "dev")
+      return error(headers, e, env.ENVIRONMENT === "dev")
     }
 
     //This should be unreachable
     return statusText(501)
   },
+}
+
+async function api(request: Request, env: CloudflareEnvironment, paths: string[]) {
+  switch (paths[2]) {
+    case "request":
+      return json(request, 200, 2)
+    case "room":
+      return fetchDurableObject(request, env.rooms, paths[3])
+    case "activity":
+      return fetchDurableObject(request, env.activityStreams, paths[3])
+  }
+
+  return statusText(404)
+}
+
+async function pageStreamProxy(url: string) {
+  const request = await fetch(url)
+  
+  const contentType = request.headers.get("Content-Type")
+  if(!contentType) { return request }
+
+  const mimeType = MimeType.parse(contentType)
+  if(!mimeType || mimeType.essence !== "text/html") { return request }
+  
+  return new HTMLRewriter()
+      .on("head", {
+          element(e) {
+              e.prepend(
+                  "<script>alert('Hello, asd!')</script>",
+                  {html: true}
+              )
+          }
+      })
+      .transform(request)
 }
 
 function fetchDurableObject(request: Request, namespace: DurableObjectNamespace, idParam?: string) {
