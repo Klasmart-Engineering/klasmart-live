6142dcd7c6accedf0d4d5e07af6745bae9503675
Owen Delahoy <owen.delahoy@gmail.com>
2021-09-02 19:42:40 +0900

    wip: jwt check

--- packages/edge-api/src/cloudflare-env.d.ts
+++ packages/edge-api/src/cloudflare-env.d.ts
@@ -3,6 +3,10 @@ export { }
 declare global {
   interface CloudflareEnvironment {
     ENVIRONMENT?: string
+    JKWS_URL?: string
+    JKWS_ISSUER?: string
+    JKWS_AUDIENCE?: string
+
     activity?: DurableObjectNamespace
     room?: DurableObjectNamespace
     wstest?: DurableObjectNamespace
--- packages/edge-api/src/room/room.ts
+++ packages/edge-api/src/room/room.ts
@@ -1,8 +1,11 @@
+import { RoomClient } from "./room-client"
 import { debugResponse } from "../responses/error"
 import { json } from "../responses/json"
 import { statusText } from "../responses/statusText"
 import { websocketUpgrade } from "../responses/websocket"
-import { RoomClient } from "./room-client"
+import { parse as parseCookies } from "cookie";
+import { createRemoteJWKSet } from "jose-browser-runtime/jwks/remote"
+import { jwtVerify } from "jose-browser-runtime/jwt/verify"
 
 export class Room implements DurableObject {
   private clients = new Map<number, RoomClient>()
@@ -12,19 +15,44 @@ export class Room implements DurableObject {
     private env: CloudflareEnvironment,
     public readonly id = state.id.toString(),
     private DEBUG = env.ENVIRONMENT === "dev",
-  ) {
+    private JWKS = env.JKWS_URL ? createRemoteJWKSet(new URL(env.JKWS_URL)) : undefined
+    ) {
     setInterval(() => {
       this.clients.forEach((client) => client.checkForTimeout(5000))
     },1000)
   }
+
+  private async authenticate(jwt?: string) {
+    if(!jwt) { return }
+    if(!this.JWKS) { return }
+
+    try {
+      const result = await jwtVerify(jwt, this.JWKS, {
+        issuer: this.env.JKWS_ISSUER,
+        audience: this.env.JKWS_AUDIENCE,
+      })
+      return result
+    } catch(e) {
+      return undefined
+    }
+  }
+
   public async fetch(request: Request): Promise<Response> {
     const { headers } = request
     try {
+      const cookieHeader = headers.get("Cookie")
+      if(!cookieHeader) { return statusText(401) }
+      const cookies = parseCookies(cookieHeader)
+
+      const auth = await this.authenticate(cookies["CF_Authorization"])
+      if(!auth) { return statusText(403) }
+
+
       if (headers.get('Upgrade') === 'websocket') {
         const protocol = headers.get('Sec-WebSocket-Protocol')
-        if (protocol === "live") { return this.accept() }
+        if (protocol === "live") { return this.accept(auth) }
       }
-      return this.status(request)
+      return this.status(request, auth)
     } catch (e) {
       return debugResponse(headers, e, this.DEBUG)
     }
@@ -33,7 +61,7 @@ export class Room implements DurableObject {
   }
 
   private nextClientId = 0
-  private accept(): Response {
+  private accept(auth: JWTVerifyResult): Response {
     const { response, ws } = websocketUpgrade("live")
 
     const id = this.nextClientId++;
@@ -43,9 +71,9 @@ export class Room implements DurableObject {
     return response
   }
 
-  private status(request: Request) {
+  private status(request: Request, auth: JWTVerifyResult) {
     const websockets = [...this.clients.entries()].map(([id]) => id)
-    return json({ websockets, id: this.state.id.toString(), this: this, request }, 200, 2)
+    return json({ auth, websockets, id: this.state.id.toString(), this: this, request }, 200, 2)
   }
 
 
