f268f2cca9bd5fd29336a5928cae4b8fa7c5cc1f
James Grant <james.grant@calmid.com>
2021-09-08 03:12:21 -0500

    fix: rebase

--- packages/edge-api/src/main.ts
+++ packages/edge-api/src/main.ts
@@ -1,20 +1,25 @@
-import { debugResponse  } from './responses/debug';
+import { debugResponse } from './responses/debug';
 import { json } from './responses/json';
 import { redirectProtocol } from './responses/redirect';
 import { statusText } from './responses/statusText';
 import MimeType from 'whatwg-mimetype';
 
-
 export default {
-  async fetch(request: Request, env: CloudflareEnvironment, ctx: unknown): Promise<Response> {
+  async fetch(
+    request: Request,
+    env: CloudflareEnvironment,
+    ctx: unknown
+  ): Promise<Response> {
     const { headers } = request;
     try {
       const url = new URL(request.url);
-      if (url.protocol === 'http:') { return redirectProtocol(url, 'https:'); }
+      if (url.protocol === 'http:') {
+        return redirectProtocol(url, 'https:');
+      }
 
       const paths = url.pathname.split('/');
 
-      switch(paths[1]) {
+      switch (paths[1]) {
         case 'api':
           return await api(request, env, ctx, paths);
         case 'h5p':
@@ -26,20 +31,26 @@ export default {
     } catch (e) {
       return debugResponse(headers, e, env.ENVIRONMENT === 'dev');
     }
-
-    //This should be unreachable
-    return statusText(501);
   },
 };
 
-async function api(request: Request, env: CloudflareEnvironment, ctx: unknown, paths: string[]) {
+async function api(
+  request: Request,
+  env: CloudflareEnvironment,
+  ctx: unknown,
+  paths: string[]
+) {
   switch (paths[2]) {
     case 'env':
-      return json({
-        env,
-        ctx,
-        ctx2: Object.getOwnPropertySymbols(ctx),
-      }, 200, 2);
+      return json(
+        {
+          env,
+          ctx,
+          ctx2: Object.getOwnPropertySymbols(ctx),
+        },
+        200,
+        2
+      );
     case 'request':
       return json(request, 200, 2);
     case 'room':
@@ -55,27 +66,37 @@ async function api(request: Request, env: CloudflareEnvironment, ctx: unknown, p
 
 async function pageStreamProxy(url: string) {
   const request = await fetch(url);
-  
+
   const contentType = request.headers.get('Content-Type');
-  if(!contentType) { return request; }
+  if (!contentType) {
+    return request;
+  }
 
   const mimeType = MimeType.parse(contentType);
-  if(!mimeType || mimeType.essence !== 'text/html') { return request; }
-  
+  if (!mimeType || mimeType.essence !== 'text/html') {
+    return request;
+  }
+
   return new HTMLRewriter()
-      .on('head', {
-          element(e) {
-              e.prepend(
-                  "<script src='/report.js' type='application/javascript'></script>",
-                  {html: true}
-              );
-          }
-      })
-      .transform(request);
+    .on('head', {
+      element(e) {
+        e.prepend(
+          "<script src='/report.js' type='application/javascript'></script>",
+          { html: true }
+        );
+      },
+    })
+    .transform(request);
 }
 
-async function fetchDurableObject(request: Request, namespace?: DurableObjectNamespace, idParam?: string) {
-  if(!namespace) { return statusText(502); }
+async function fetchDurableObject(
+  request: Request,
+  namespace?: DurableObjectNamespace,
+  idParam?: string
+) {
+  if (!namespace) {
+    return statusText(502);
+  }
   const id = idParam
     ? namespace.idFromString(idParam)
     : namespace.newUniqueId();
