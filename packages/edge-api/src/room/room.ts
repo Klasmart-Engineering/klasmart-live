5c03d44b13fcafdec8f30737cb8ba19932e2d5f5
James Grant <james.grant@calmid.com>
2021-09-08 23:21:27 -0500

    wip: simplify room logic, in order to debug broken websockets
    Websockets now work again

--- packages/edge-api/src/room/room.ts
+++ packages/edge-api/src/room/room.ts
@@ -36,202 +36,243 @@ export interface Token {
 const DEBOUNCE_TIME = 300;
 
 export class Room implements DurableObject {
-  public store: EnhancedStore;
+  // public store: EnhancedStore;
 
-  private clients = new Map<string, UserDevices>();
-  private teachers = new Set<string>();
+  // private clients = new Map<string, UserDevices>();
+  // private teachers = new Set<string>();
 
-  private deviceId = 0;
-  private fanOutDebounceTimeout: number | undefined = undefined;
-  private lastFanOut = Date.now();
-  private previousState = INITIAL_ROOM_STATE;
+  // private deviceId = 0;
+  // private fanOutDebounceTimeout: number | undefined = undefined;
+  // private lastFanOut = Date.now();
+  // private previousState = INITIAL_ROOM_STATE;
+
+  private numMessages = 0;
 
   public constructor(
     private state: DurableObjectState,
-    private env: CloudflareEnvironment,
-    private DEBUG = env.ENVIRONMENT === 'dev',
-    private JWKS = env.JKWS_URL
-      ? createRemoteJWKSet(new URL(env.JKWS_URL))
-      : undefined
+    private env: CloudflareEnvironment
+    // private state: DurableObjectState,
+    // private env: CloudflareEnvironment,
+    // private DEBUG = env.ENVIRONMENT === 'dev',
+    // private JWKS = env.JKWS_URL
+    //   ? createRemoteJWKSet(new URL(env.JKWS_URL))
+    //   : undefined
   ) {
-    this.store = configureStore({
-      middleware: [
-        () => (next) => (action) => {
-          // Check that we haven't debounced for longer than 1 second already
-          if (
-            Date.now() - this.lastFanOut > 1000 &&
-            this.fanOutDebounceTimeout
-          ) {
-            next(action);
-            return;
-          }
-          // If we didn't previously have a fan out triggered, then trigger one
-          if (!this.fanOutDebounceTimeout) {
-            this.triggerFanOut();
-            next(action);
-            return;
-          }
+  //   this.store = configureStore({
+  //     middleware: [
+  //       () => (next) => (action) => {
+  //         // Check that we haven't debounced for longer than 1 second already
+  //         if (
+  //           Date.now() - this.lastFanOut > 1000 &&
+  //           this.fanOutDebounceTimeout
+  //         ) {
+  //           next(action);
+  //           return;
+  //         }
+  //         // If we didn't previously have a fan out triggered, then trigger one
+  //         if (!this.fanOutDebounceTimeout) {
+  //           this.triggerFanOut();
+  //           next(action);
+  //           return;
+  //         }
 
-          // If we have a timeout triggered then clear and restart the debounce
-          clearTimeout(this.fanOutDebounceTimeout);
-          next(action);
-          this.triggerFanOut();
-        },
-      ],
-      reducer: {
-        room: roomReducer,
-      },
-    });
-    this.previousState = this.currentState;
+  //         // If we have a timeout triggered then clear and restart the debounce
+  //         clearTimeout(this.fanOutDebounceTimeout);
+  //         next(action);
+  //         this.triggerFanOut();
+  //       },
+  //     ],
+  //     reducer: {
+  //       room: roomReducer,
+  //     },
+  //   });
+  //   this.previousState = this.currentState;
   }
 
-  get currentState(): IState {
-    return this.store.getState().room;
-  }
+  // get currentState(): IState {
+  //   return this.store.getState().room;
+  // }
 
   public async fetch(request: Request): Promise<Response> {
-    const { headers } = request;
-    try {
-      const cookieHeader = headers.get('Cookie');
-      if (!cookieHeader) {
-        return statusText(401);
-      }
-      const cookies = parseCookies(cookieHeader);
+    console.log('hey');
+    const protocol = request.headers.get('Sec-WebSocket-Protocol');
 
-      const token = await this.authenticate(cookies['CF_Authorization']);
-      if (!token) {
-        return statusText(403);
-      }
-      const { email } = token;
-      if (typeof email !== 'string') {
-        return statusText(400);
-      }
+    const webSocketPair = new WebSocketPair();
+    const headers =
+      typeof protocol === 'string'
+        ? { 'Sec-WebSocket-Protocol': protocol }
+        : undefined;
 
-      if (headers.get('Upgrade') === 'websocket') {
-        const protocol = headers.get('Sec-WebSocket-Protocol')?.toLowerCase();
-        if (protocol === 'live') {
-          return this.accept(token);
-        }
-      }
-      return this.status(request, token);
-    } catch (e) {
-      return debugResponse(headers, e, this.DEBUG);
-    }
-  }
-
-  public disconnectDevice(device: Device): void {
-    const userDevices = this.getUserDevices(device.context.userId);
-    userDevices.delete(device.deviceId);
-  }
-
-  public fanOutStateDiff(): void {
-    const latestState = this.currentState;
-    const diff: IStateChanges = {
-      changes: generateStateDiff(this.previousState, latestState),
-    };
-
-    Object.values(this.clients).forEach((devices: UserDevices) => {
-      Object.values(devices).forEach((device: Device) =>
-        device.sendStateDiff(diff)
-      );
+    const ws = webSocketPair[1];
+    ws.addEventListener('close', () => console.log('close'));
+    ws.addEventListener('message', ({data}) => {
+      console.log(`message: ${data}`);
+      this.numMessages++;
+      const message = JSON.stringify({
+        id,
+        numMessages: this.numMessages,
+        state: [...Object.entries(this.state)],
+      });
+      ws.send(message);
     });
+    ws.addEventListener('error', () => console.log('error'));
+    ws.accept();
 
-    this.previousState = latestState;
-    this.lastFanOut = Date.now();
-  }
-
-  // move to auth.ts
-  private async authenticate(jwt?: string): Promise<Token> {
-    if (!jwt) {
-      throw new Error('No JWT provided');
-    }
-    if (!this.JWKS) {
-      throw new Error('JWT Decoding information not found');
-    }
-
-    const result = await jwtVerify(jwt, this.JWKS, {
-      issuer: this.env.JKWS_ISSUER,
-      audience: this.env.JKWS_AUDIENCE,
+    const id = this.state.id.toString();
+    const message = JSON.stringify({
+      id,
+      numMessages: this.numMessages,
+      state: [...Object.entries(this.state)],
     });
-    return (result.payload as unknown) as Token;
+    ws.send(message);
+
+    return new Response(null, {
+      status: 101,
+      webSocket: webSocketPair[0],
+      headers,
+    });
+    // const { headers } = request;
+    // try {
+    //   const cookieHeader = headers.get('Cookie');
+    //   if (!cookieHeader) {
+    //     return statusText(401);
+    //   }
+    //   const cookies = parseCookies(cookieHeader);
+
+    //   const token = await this.authenticate(cookies['CF_Authorization']);
+    //   if (!token) {
+    //     return statusText(403);
+    //   }
+    //   const { email } = token;
+    //   if (typeof email !== 'string') {
+    //     return statusText(400);
+    //   }
+
+    //   if (headers.get('Upgrade') === 'websocket') {
+    //     const protocol = headers.get('Sec-WebSocket-Protocol')?.toLowerCase();
+    //     if (protocol === 'live') {
+    //       return this.accept(token);
+    //     }
+    //   }
+    //   return this.status(request, token);
+    // } catch (e) {
+    //   return debugResponse(headers, e, this.DEBUG);
+    // }
   }
 
-  private getUserDevices(userId: string): UserDevices {
-    const user = this.clients.get(userId);
-    if (user === undefined) {
-      const newUser = new Map();
-      this.clients.set(userId, newUser);
-      return newUser;
-    }
-    return user;
-  }
+//   public disconnectDevice(device: Device): void {
+//     const userDevices = this.getUserDevices(device.context.userId);
+//     userDevices.delete(device.deviceId);
+//   }
 
-  private accept(token: Token): Response {
-    const { response, ws } = websocketUpgrade('live');
+//   public fanOutStateDiff(): void {
+//     const latestState = this.currentState;
+//     const diff: IStateChanges = {
+//       changes: generateStateDiff(this.previousState, latestState),
+//     };
 
-    const userId = token.sub;
-    const deviceId = this.deviceId++;
-    const context = tokenToContext(token);
-    const device = new Device(
-      deviceId,
-      context,
-      ws,
-      this,
-      this.env.ENVIRONMENT === 'dev'
-    );
-    const userDevices = this.getUserDevices(userId);
-    // @TODO can we make the device id deterministic somehow?
-    // Eg. If a user connects with their iPad, then disconnects and reconnects,
-    // is there a way we can ensure that that device is consistently assigned
-    // the same ID. Eg. Mac Address? Keeping an ID in local storage?
-    userDevices.set(deviceId, device);
-    const userJoinAction = {
-      context,
-      payload: {},
-    };
+//     Object.values(this.clients).forEach((devices: UserDevices) => {
+//       Object.values(devices).forEach((device: Device) =>
+//         device.sendStateDiff(diff)
+//       );
+//     });
 
-    this.store.dispatch(Actions.userJoin(userJoinAction));
+//     this.previousState = latestState;
+//     this.lastFanOut = Date.now();
+//   }
 
-    const setDeviceAction = {
-      context,
-      payload: {
-        deviceId: deviceId.toString(),
-      },
-    };
-    this.store.dispatch(Actions.setDevice(setDeviceAction));
+//   // move to auth.ts
+//   private async authenticate(jwt?: string): Promise<Token> {
+//     if (!jwt) {
+//       throw new Error('No JWT provided');
+//     }
+//     if (!this.JWKS) {
+//       throw new Error('JWT Decoding information not found');
+//     }
 
-    return response;
-  }
+//     const result = await jwtVerify(jwt, this.JWKS, {
+//       issuer: this.env.JKWS_ISSUER,
+//       audience: this.env.JKWS_AUDIENCE,
+//     });
+//     return (result.payload as unknown) as Token;
+//   }
 
-  private triggerFanOut(): void {
-    this.fanOutDebounceTimeout = setTimeout(
-      () => this.fanOutStateDiff(),
-      DEBOUNCE_TIME
-    );
-  }
+//   private getUserDevices(userId: string): UserDevices {
+//     const user = this.clients.get(userId);
+//     if (user === undefined) {
+//       const newUser = new Map();
+//       this.clients.set(userId, newUser);
+//       return newUser;
+//     }
+//     return user;
+//   }
 
-  /**
-   THIS MUST BE DELETED
-  */
-  private status(request: Request, auth: Token) {
-    const clients = [...this.clients.entries()].map(
-      ([id, devices]) =>
-        `${id}: ${[...devices.entries()].map(([id]) => `Device: ID ${id}\n`)}`
-    );
-    return json(
-      { auth, clients, id: this.state.id.toString(), this: this, request },
-      200,
-      2
-    );
-  }
+//   private accept(token: Token): Response {
+//     const { response, ws } = websocketUpgrade('live');
+
+//     const userId = token.sub;
+//     const deviceId = this.deviceId++;
+//     const context = tokenToContext(token);
+//     const device = new Device(
+//       deviceId,
+//       context,
+//       ws,
+//       this,
+//       this.env.ENVIRONMENT === 'dev'
+//     );
+//     const userDevices = this.getUserDevices(userId);
+//     // @TODO can we make the device id deterministic somehow?
+//     // Eg. If a user connects with their iPad, then disconnects and reconnects,
+//     // is there a way we can ensure that that device is consistently assigned
+//     // the same ID. Eg. Mac Address? Keeping an ID in local storage?
+//     userDevices.set(deviceId, device);
+//     const userJoinAction = {
+//       context,
+//       payload: {},
+//     };
+
+//     this.store.dispatch(Actions.userJoin(userJoinAction));
+
+//     const setDeviceAction = {
+//       context,
+//       payload: {
+//         deviceId: deviceId.toString(),
+//       },
+//     };
+//     this.store.dispatch(Actions.setDevice(setDeviceAction));
+
+//     return response;
+//   }
+
+//   private triggerFanOut(): void {
+//     this.fanOutDebounceTimeout = setTimeout(
+//       () => this.fanOutStateDiff(),
+//       DEBOUNCE_TIME
+//     );
+//   }
+
+//   /**
+//    THIS MUST BE DELETED
+//   */
+//   private status(request: Request, auth: Token) {
+//     const clients = [...this.clients.entries()].map(
+//       ([id, devices]) =>
+//         `${id}: ${[...devices.entries()].map(([id]) => `Device: ID ${id}\n`)}`
+//     );
+//     return json(
+//       { auth, clients, id: this.state.id.toString(), this: this, request },
+//       200,
+//       2
+//     );
+//   }
 }
 
-// A helper function until the code is re-written to use
-// Kidsloop Authentication token
-function tokenToContext(token: Token): Context {
-  return {
-    userId: token.sub,
-    isTeacher: false,
-    name: token.email,
-  };
-}
+// // A helper function until the code is re-written to use
+// // Kidsloop Authentication token
+// function tokenToContext(token: Token): Context {
+//   return {
+//     userId: token.sub,
+//     isTeacher: false,
+//     name: token.email,
+//   };
+// }
--- packages/edge-site/pages/room-test.tsx
+++ packages/edge-site/pages/room-test.tsx
@@ -1,56 +1,44 @@
-import { GetStaticProps } from "next";
-import { Provider, useSelector } from "react-redux";
-import { store } from "kidsloop-live-state";
-import { useEffect } from "react";
+import Head from 'next/head'
+import { GetStaticProps } from 'next'
+import { useEffect, useMemo, useState } from 'react'
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+
 
 export const getStaticProps: GetStaticProps = async () => {
   return {
-    props: {},
-  };
-};
-
-export default function RoomTest() {
-  let ws: WebSocket;
-  useEffect(() => {
-    const roomId = window ? window.location.hash.slice(1) : undefined;
-    const url = new URL("wss://live.kidsloop.dev/api/room");
-    if (roomId) {
-      url.hostname += `/${roomId}`;
-    }
-    ws = new WebSocket(url.toString(), ["live"]);
-    ws.binaryType = "arraybuffer";
-    ws.addEventListener("open", (evt) => {
-      console.log("On Open", evt);
-    });
-    ws.addEventListener("message", (evt) => {
-      console.log("On message", evt);
-    });
-    ws.addEventListener("close", (evt) => {
-      console.log("On close", evt);
-    });
-    ws.addEventListener("error", (evt) => {
-      console.log("On error", evt);
-    });
-    console.log(ws);
-  }, []);
-
-  return (
-    <Provider store={store}>
-      <ShowState />
-    </Provider>
-  );
+    props: { }
+  }
 }
 
-function ShowState() {
-  const state = useSelector((s) => s)["room"];
-  return (
-    <>
-      <h1>Room State</h1>
-      {Object.entries(state).map(([k, v]) => (
-        <div>
-          <b>{JSON.stringify(k).replaceAll('"', "")}</b>: {JSON.stringify(v)}
-        </div>
-      ))}
-    </>
-  );
+let heartbeatHandler;
+
+export default function Home() {
+  const url = "wss://live.kidsloop.dev/api/room";
+
+  const {
+    sendMessage,
+    lastMessage,
+    readyState,
+  } = useWebSocket(url, {
+    onOpen: () => {
+      heartbeatHandler = setInterval(() => sendMessage('heartbeat'), 1000);
+    },
+    onClose: () => {
+      clearInterval(heartbeatHandler);
+    }
+  });
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  return <>
+    Status: {connectionStatus}
+    <br/>
+    { lastMessage?.data }
+  </>
 }
