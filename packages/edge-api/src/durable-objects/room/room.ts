43477e45f51da492bb85942361217d74bc5e71d7
James Grant <james.grant@calmid.com>
2021-10-07 21:55:43 -0500

    wip: begin simplification of room durable object

--- packages/edge-api/src/durable-objects/room/room.ts
+++ packages/edge-api/src/durable-objects/room/room.ts
@@ -1,56 +1,16 @@
-import { debugResponse } from '../responses/debug';
-import { statusText } from '../responses/statusText';
-import { Device } from './device';
-import {
-  configureStore,
-  Dispatch,
-  EnhancedStore,
-  Middleware,
-} from '@reduxjs/toolkit';
-import { Server, Context } from 'kidsloop-live-state';
-import * as pb from 'kidsloop-live-state/dist/protobuf';
-import { websocketUpgrade } from '../responses/websocket';
-import { authenticate } from '../utils/auth';
-import { isError } from '../utils/result';
-import { json } from '../responses/json';
+import { debugResponse } from '../../responses/debug';
+import { json } from '../../responses/json';
+import { statusText } from '../../responses/statusText';
+import { websocketUpgrade } from '../../responses/websocket';
+import { authenticate } from '../../utils/auth';
+import { isError } from '../../utils/result';
+import { nanoid } from 'nanoid';
+import { ClassRequest } from 'kidsloop-live-state/dist/protobuf';
 
-const { roomReducer, Actions, INITIAL_ROOM_STATE, generateStateDiff } = Server;
-
-type UserDevices = Map<string, Device>;
-
-const loggerMiddleware: Middleware<Dispatch> = (store) => (next) => (
-  action
-) => {
-  console.log('dispatching: ' + JSON.stringify(action));
-  const result = next(action);
-  console.log('next state: ' + JSON.stringify(store.getState().room));
-  return result;
-};
-
-const createFanOutMiddleware = (room: Room): Middleware<Dispatch> => {
-  const fanOutMiddleware: Middleware<Dispatch> = () => (next) => (action) => {
-    if (Date.now() - room.lastFanOut < 750) {
-      if (room.fanOutDebounceTimeout !== undefined)
-        clearTimeout(room.fanOutDebounceTimeout);
-    }
-    room.triggerFanOut();
-    next(action);
-  };
-  return fanOutMiddleware;
-};
-
-const DEBOUNCE_TIME = 300;
 
 export class Room implements DurableObject {
-  public store: EnhancedStore;
-  public fanOutDebounceTimeout: number | undefined = undefined;
-  public lastFanOut = Date.now();
 
-  private connectedDevices = new Map<string, UserDevices>();
-  private teachers = new Set<string>();
-
-  private deviceId = 0;
-  private previousState: pb.IState;
+  private clients: Map<string, CloudflareWebsocket>;
 
   public constructor(
     private readonly state: DurableObjectState,
@@ -62,26 +22,7 @@ export class Room implements DurableObject {
       audience: env.JKWS_AUDIENCE,
     },
   ) {
-    this.store = configureStore({
-      middleware: [
-        createFanOutMiddleware(this),
-        ...(DEBUG ? [loggerMiddleware] : []),
-      ],
-      reducer: {
-        room: roomReducer,
-      },
-      preloadedState: {
-        room: {
-          ...INITIAL_ROOM_STATE,
-          roomId: this.state.id.toString(),
-        },
-      },
-    });
-    this.previousState = this.currentState;
-  }
-
-  get currentState(): pb.IState {
-    return this.store.getState().room;
+    this.clients = new Map();
   }
 
   public async fetch(request: Request): Promise<Response> {
@@ -92,90 +33,54 @@ export class Room implements DurableObject {
         return statusText(400, 'Please connect to this endpoint via websocket');
       }
 
-      const authenticationResult = await authenticate(request, this.JWKS_URL, this.jwtOptions);
-      if(isError(authenticationResult)) { return authenticationResult.payload; }
-      const context = authenticationResult.payload;
+      // const authenticationResult = await authenticate(request, this.JWKS_URL, this.jwtOptions);
+      // if(isError(authenticationResult)) { return authenticationResult.payload; }
+      // const context = authenticationResult.payload;
 
       const protocol = request.headers.get('Sec-WebSocket-Protocol');
       const { response, ws } = websocketUpgrade(protocol);
       
-      this.addNewDevice(context, ws);
+      this.setupWebsocket(ws);
       return response;
     } catch (e) {
       return debugResponse(headers, e, this.DEBUG);
     }
   }
 
-  /**
-   * Triggers the room to send out the latest state changes to all connected
-   * clients that have occurred since the last fan out
-   *
-   * The actual fan out is debounced
-   */
-  public triggerFanOut(): void {
-    this.fanOutDebounceTimeout = setTimeout(() => {
-      const latestState = this.currentState;
-      const diff: pb.IStateChanges = {
-        changes: generateStateDiff(this.previousState, latestState),
-      };
-      if ((diff.changes?.length || 0) > 0) {
-        const bytes = pb.StateChanges.encode(diff).finish();
-        [...this.connectedDevices.values()].forEach((devices: UserDevices) => {
-          [...devices.values()].forEach((device: Device) =>
-            device.sendProtobufBytes(bytes)
-          );
-        });
-      }
-      this.previousState = latestState;
-      this.lastFanOut = Date.now();
-      this.fanOutDebounceTimeout = undefined;
-    }, DEBOUNCE_TIME);
+  private setupWebsocket(ws: CloudflareWebsocket) {
+    const id = nanoid();
+    this.clients.set(id, ws);
+    ws.addEventListener('close', () => this.onWsClose(ws, id));
+    ws.addEventListener('message', ({data}) => this.onWsMessage(ws, id, data));
+    ws.addEventListener('error', () => this.onWsError(ws, id));
+    ws.accept();
   }
 
-  public disconnectDevice(device: Device): void {
-    const userDevices = this.getUserDevices(device.context.userId);
-    userDevices.delete(device.deviceId);
-  }
-
-
-  /**
-   * Creates and adds a new device to both the redux store and the internal list
-   * of connected client devices
-   */
-  private addNewDevice(context: Context, ws: CloudflareWebsocket): void {
-    const deviceId = `${this.deviceId++}`;
-    const { userId } = context;
-    const device = new Device(deviceId, context, ws, this, this.DEBUG);
-
-    this.setUserDevice(userId, device);
-
-    const userJoinAction = {
-      context,
-      payload: {},
-    };
-
-    this.store.dispatch(Actions.userJoin(userJoinAction));
-
-    const setDeviceAction = {
-      context,
-      payload: {
-        deviceId,
-        device: {},
-      },
-    };
-    this.store.dispatch(Actions.setDevice(setDeviceAction));
-  }
-
-  private getUserDevices(userId: string): UserDevices {
-    if (!this.connectedDevices.has(userId)) {
-      this.connectedDevices.set(userId, new Map());
+  private onWsMessage(ws: CloudflareWebsocket, id: string, data: ArrayBuffer | string) {
+    if (!(data instanceof ArrayBuffer)) {
+      console.log('recieved non ArrayBuffer data: ' + data);
+      this.onWsClose(ws, id);
+      return;
     }
-    return this.connectedDevices.get(userId)!;
+
+    const request = ClassRequest.decode(new Uint8Array(data));
+    switch (request.type) {
+      case undefined:
+        console.log('request has no type. this should never happen');
+        return;
+      // other cases...
+    }
   }
 
-  private setUserDevice(userId: string, device: Device): void {
-    const userDevices = this.getUserDevices(userId);
-    userDevices.set(device.deviceId, device);
+  private onWsClose(ws: CloudflareWebsocket, id: string) {
+    console.log('ws closed: ' + id);
+    this.clients.delete(id);
   }
+
+  private onWsError(ws: CloudflareWebsocket, id: string) {
+    console.log('ws errored: ' + id);
+    this.clients.delete(id);
+  }
+
 }
 
