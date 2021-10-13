fff8bf47843bd4c440fe1d3c2fc60ddda5f27904
James Grant <james.grant@calmid.com>
2021-10-12 22:43:32 -0500

    feat: broadcast messages to all clients and dispatch actions

--- packages/edge-api/src/durable-objects/room/room.ts
+++ packages/edge-api/src/durable-objects/room/room.ts
@@ -5,26 +5,21 @@ import { websocketUpgrade } from '../../responses/websocket';
 import { authenticate, Context } from '../../utils/auth';
 import { isError } from '../../utils/result';
 import { nanoid } from 'nanoid';
-import { classReducer, newDeviceId, pb } from 'kidsloop-live-state';
+import { ClassAction, classReducer, ClassState, convertCommandToEvent, DeviceID, messageToClassAction, newDeviceId, pb } from 'kidsloop-live-state';
 import { configureStore, EnhancedStore } from '@reduxjs/toolkit';
-import { ClassMessage } from 'kidsloop-live-state/dist/protobuf';
 
 export class Room implements DurableObject {
 
-  public fanOutDebounceTimeout: number | undefined = undefined;
-  public lastFanOut = Date.now();
-
-  private clients = new Map<string, CloudflareWebsocket>();
-  private store: EnhancedStore
+  private clients = new Map<DeviceID, CloudflareWebsocket>();
+  private store: EnhancedStore<ClassState, ClassAction>;
 
   public constructor(
     private readonly state: DurableObjectState,
     private readonly env: CloudflareEnvironment,
     private readonly DEBUG = env.ENVIRONMENT === 'dev',
   ) {
-    this.store = configureStore( {
+    this.store = configureStore<ClassState, ClassAction>( {
       reducer: classReducer,
-      middleware: [],
     });
   }
 
@@ -62,11 +57,11 @@ export class Room implements DurableObject {
     this.clients.set(deviceId, ws);
 
     ws.addEventListener('close', () => this.onWsClose(ws, deviceId));
-    ws.addEventListener('message', ({ data }) => this.onWsMessage(ws, deviceId, data));
+    ws.addEventListener('message', ({ data }) => this.onWsMessage(ws, data, deviceId, context));
     ws.addEventListener('error', () => this.onWsError(ws, deviceId));
     ws.accept();
 
-    const connectEvent: pb.IDeviceConnectedEvent = {
+    const event: pb.IDeviceConnectedEvent = {
       name: context.name,
       role: context.role,
       device: {
@@ -74,36 +69,74 @@ export class Room implements DurableObject {
         userId: context.userId,
       }
     };
-    // this.store.dispatch()
-  }
-
-  private onWsMessage(ws: CloudflareWebsocket, id: string, data: ArrayBuffer | string) {
-    if (!(data instanceof ArrayBuffer)) {
-      console.log('recieved non ArrayBuffer data: ' + data);
-      this.onWsClose(ws, id);
+    const message: pb.IClassMessage = {deviceConnected: event};
+    const action = messageToClassAction(message);
+    if (!action) {
+      console.log('parsing message into redux action failed');
       return;
     }
-    const requestWrapper = ClassMessage.decode(new Uint8Array(data));
-    // this.store.dispatch()
+    this.store.dispatch(action);
+    this.broadcast(message);
   }
 
-  private onWsClose(ws: CloudflareWebsocket, id: string) {
-    console.log('ws closed: ' + id);
-    this.clients.delete(id);
-    // DisconnectDevice
-    // this.store.dispatch()
+  private onWsMessage(ws: CloudflareWebsocket, data: ArrayBuffer | string, deviceId: DeviceID, context: Context) {
+    if (!(data instanceof ArrayBuffer)) {
+      console.log('recieved non ArrayBuffer data: ' + data);
+      this.onWsClose(ws, deviceId);
+      return;
+    }
+    const request = pb.ClassRequest.decode(new Uint8Array(data));
+    const message = convertCommandToEvent(request, context.userId, deviceId);
+    if (!message) {
+      console.log('parsing request into message failed');
+      return;
+    }
+    const action = messageToClassAction(message);
+    if (!action) {
+      console.log('parsing message into redux action failed');
+      return;
+    }
+    this.store.dispatch(action);
+    this.broadcast(message);
   }
 
-  private onWsError(ws: CloudflareWebsocket, id: string) {
-    console.log('ws errored: ' + id);
-    this.clients.delete(id);
-    // DisconnectDevice
-    // this.store.dispatch()
+  private onWsClose(ws: CloudflareWebsocket, deviceId: DeviceID) {
+    console.log('ws closed: ' + deviceId);
+    this.clients.delete(deviceId);
+    const event: pb.IDeviceDisconnectedEvent = {
+      deviceId: deviceId,
+    };
+    const message: pb.IClassMessage = {deviceDisconnected: event};
+    const action = messageToClassAction(message);
+    if (!action) {
+      console.log('parsing message into redux action failed');
+      return;
+    }
+    this.store.dispatch(action);
+    this.broadcast(message);
   }
 
-  private broadcast() {
-    throw new Error('Not implemented');
+  private onWsError(ws: CloudflareWebsocket, deviceId: DeviceID) {
+    console.log('ws errored: ' + deviceId);
+    this.clients.delete(deviceId);
+    const event: pb.IDeviceDisconnectedEvent = {
+      deviceId: deviceId,
+    };
+    const message: pb.IClassMessage = {deviceDisconnected: event};
+    const action = messageToClassAction(message);
+    if (!action) {
+      console.log('parsing message into redux action failed');
+      return;
+    }
+    this.store.dispatch(action);
+    this.broadcast(message);
+  }
+
+  private broadcast(message: pb.IClassMessage) {
+    [...this.clients.values()].forEach((client) => {
+      const bytes = pb.ClassMessage.encode(message).finish();
+      client.send(bytes);
+    });
   }
 
 }
-
