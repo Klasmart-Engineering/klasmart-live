28a4e3b0896b4e3e5075854f89a22f179f8bbe2e
Owen Delahoy <owen.delahoy@gmail.com>
2021-11-22 15:27:54 +0900

    chore(state): move state to submodule

--- packages/state/dist/react/provider.d.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import { State } from "../ui";
-export interface NetworkProviderProps<ApplicationState = unknown> {
-    children?: ReactNode;
-    selector: (state: ApplicationState) => State;
-}
-export declare function NetworkProvider<ApplicationState = unknown>({ children, selector, }: NetworkProviderProps<ApplicationState>): JSX.Element;
-//# sourceMappingURL=provider.d.ts.map
--- packages/state/src/react/hooks/state.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import NetworkContext from '../context';
-import { useSelector } from 'react-redux';
-import { State } from '../../redux/reducer';
-
-export function useNetworkState<T=unknown>(selector: (state: State) => T) {
-  const network = useContext(NetworkContext);
-  return useSelector((state) => selector(network.selector(state)))
-}
